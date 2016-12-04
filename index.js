bf57ee36036cd91b89bac9eb7845e324addb985e
khaled hossain <md.khaled.eee@gmail.com>
2016-12-04 17:14:50 +0600

    pug integrated

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
+//pug & ejs does not require to call explicitely, express does it internally
+app.set("views", "./views");
+
 app.use(express.static('public'));
 app.use(express.static("node_modules/bootstrap/dist"));
+
+app.get('/rooms', (req, res, next)=>{
+    res.render("rooms.pug");
+})
 
 server.listen(3000, ()=>{
     console.log("server on port 3000 running");
