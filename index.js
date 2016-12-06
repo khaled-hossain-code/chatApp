0e436783b71cff63409390dba612f802a719f1c7
khaled hossain <md.khaled.eee@gmail.com>
2016-12-06 16:20:08 +0600

    morgan added for logging

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ const server = http.createServer(app);
 const bodyParser = require("body-parser");
 const adminRouter = require("./router/adminRoute");
 const roomsApiRouter = require("./api/roomsApi");
+const fs = require("fs");
+const morgan = require("morgan");
+var accessLogStream = fs.createWriteStream(__dirname+'/access.log',{flags:'a'});
 //pug & ejs does not require to call explicitely, express does it internally
 app.set("views", "./views");
 app.set("view engine", "pug");
 
+app.use(morgan('combined',{stream:accessLogStream}));
 app.use(express.static('public'));
 app.use(express.static("node_modules/bootstrap/dist"));
 app.use(express.static("node_modules/jquery/dist"));
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="globals/body-parser/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/lodash/index.d.ts" />
+/// <reference path="globals/morgan/index.d.ts" />
 /// <reference path="globals/node-uuid/index.d.ts" />
 /// <reference path="globals/pug/index.d.ts" />
