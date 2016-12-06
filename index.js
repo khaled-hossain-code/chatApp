c180f87f96f1f41765a6bd9a7af6cdbd9652f1e6
khaled hossain <md.khaled.eee@gmail.com>
2016-12-06 15:08:17 +0600

    completed the API for chat room

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(express.static('public'));
 app.use(express.static("node_modules/bootstrap/dist"));
 app.use(express.static("node_modules/jquery/dist"));
 app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res, next) => {
     res.render("home", { title: "Home"});
--- public/home.js
+++ public/home.js
@@ -1,56 +1,62 @@
-$(function(){
+$(function () {
     var roomId;
 
     $.ajax({
         type: "GET",
         url: "/api/rooms",
-    }).done(function (rooms){
-        if (rooms[0] != null){
+    }).done(function (rooms) {
+        if (rooms[0] != null) {
             roomId = rooms[0].id;
         }
-        
+
         getMessages();
 
-        $.each(rooms, function(key,room){
+        $.each(rooms, function (key, room) {
             var a = '<a href="#" data-room-id="' + room.id + '" class="room list-group-item">' + room.name + '</a>';
             $("#rooms").append(a);
         })
     });
 
-    $("#post").click(function(){
+    $("#post").click(function () {
         var message = {
             text: $("#message").val()
         };
 
         $.ajax({
-            type:"POST",
-            url:"/api/rooms/"+ roomId + "/messages",
-            data: message
-        }).done( function(){
+            type: "POST",
+            url: "/api/rooms/" + roomId + "/messages",
+            data: JSON.stringify(message),
+            contentType: "application/json"
+        }).done(function () {
             $("#message").val("");
             getMessages();
         });
     });
 
-    $("#delete").click(function(){
+    $("#delete").click(function () {
         $.ajax({
             type: "DELETE",
             url: "/api/rooms/" + roomId + "/messages"
-        }).done(function(){
+        }).done(function () {
             $("#messages").val("");
         });
     });
 
-    function getMessages(){
+    $('body').on('click', 'a.room', function (event) {
+        roomId = $(event.target).attr("data-room-id");
+        getMessages();
+    });
+
+    function getMessages() {
         $.ajax({
-            type:"GET",
-            url: "/api/rooms/"+roomId+"/messages",
-        }).done( function(data){
-            
+            type: "GET",
+            url: "/api/rooms/" + roomId + "/messages",
+        }).done(function (data) {
+
             $("#roomName").text("Message for " + data.room.name);
             var messages = "";
 
-            $.each(data.messages, function(key,message){    
+            $.each(data.messages, function (key, message) {
                 messages += message.text + '\r';
             });
 
